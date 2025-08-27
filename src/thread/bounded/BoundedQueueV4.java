fa5f35043c510421e3d593c17270e7e71d02ec03
Lee-HyunSoo <cris_lx@naver.com>
2025-08-27 16:16:14 +0900

    생산자 소비자 문제2

---
+++ src/thread/bounded/BoundedQueueV4.java
@@ -0,0 +1,78 @@
+package thread.bounded;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import static util.MyLogger.log;
+
+public class BoundedQueueV4 implements BoundedQueue {
+
+    private final Lock lock = new ReentrantLock();
+    // Condition : ReentrantLock을 사용하는 스레드가 대기하는 공간
+    private final Condition condition = lock.newCondition();
+
+    private final Queue<String> queue = new ArrayDeque<>();
+    private final int max;
+
+    public BoundedQueueV4(int max) {
+        this.max = max;
+    }
+
+    @Override
+    public void put(String data) {
+        lock.lock();
+        try {
+            while (queue.size() == max) {
+                log("[put] 큐가 가득 참, 생산자 대기: " + data);
+                try {
+                    // RUNNABLE -> WAITING, 락 반납
+                    // 기존에는 Object.wait()을 통해 Object 내의 기본 대기소에 넣었음
+                    // 현재는 condition 이라는 대기소에 넣어줌
+                    condition.await();
+                    log("[put] 생산자 깨어남");
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            queue.offer(data);
+            log("[put] 생산자 데이터 저장, condition.signal() 호출");
+
+            // 대기 스레드, WAIT -> BLOCKED
+            // condition 내의 대기중인 스레드를 하나 깨움
+            condition.signal();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    @Override
+    public String take() {
+        lock.lock();
+
+        try {
+            while (queue.isEmpty()) {
+                log("[take] 큐에 데이터가 없음, 소비자 대기");
+                try {
+                    condition.await();
+                    log("[take] 소비자 깨어남");
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            String data = queue.poll();
+            log("[take] 소비자 데이터 획득, condition.signal() 호출");
+            condition.signal();
+            return data;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return queue.toString();
+    }
+}
---
+++ src/thread/bounded/BoundedQueueV5.java
@@ -0,0 +1,81 @@
+package thread.bounded;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import static util.MyLogger.log;
+
+public class BoundedQueueV5 implements BoundedQueue {
+
+    private final Lock lock = new ReentrantLock();
+    // Condition : ReentrantLock을 사용하는 스레드가 대기하는 공간
+    // lock.newCondition()을 호출하면 lock과 관련 된 대기 공간을 생성
+    // 하나의 lock을 통해 여러 대기공간을 만들 수도 있다.
+    private final Condition producerCond = lock.newCondition();
+    private final Condition consumerCond = lock.newCondition();
+
+    private final Queue<String> queue = new ArrayDeque<>();
+    private final int max;
+
+    public BoundedQueueV5(int max) {
+        this.max = max;
+    }
+
+    @Override
+    public void put(String data) {
+        lock.lock();
+        try {
+            while (queue.size() == max) {
+                log("[put] 큐가 가득 참, 생산자 대기: " + data);
+                try {
+                    // RUNNABLE -> WAITING, 락 반납
+                    // 기존에는 Object.wait()을 통해 Object 내의 기본 대기소에 넣었음
+                    // 현재는 condition 이라는 대기소에 넣어줌
+                    producerCond.await();
+                    log("[put] 생산자 깨어남");
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            queue.offer(data);
+            log("[put] 생산자 데이터 저장, consumerCond.signal() 호출");
+
+            // 대기 스레드, WAIT -> BLOCKED
+            // condition 내의 대기중인 스레드를 하나 깨움
+            consumerCond.signal();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    @Override
+    public String take() {
+        lock.lock();
+
+        try {
+            while (queue.isEmpty()) {
+                log("[take] 큐에 데이터가 없음, 소비자 대기");
+                try {
+                    consumerCond.await();
+                    log("[take] 소비자 깨어남");
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            String data = queue.poll();
+            log("[take] 소비자 데이터 획득, producerCond.signal() 호출");
+            producerCond.signal();
+            return data;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return queue.toString();
+    }
+}
